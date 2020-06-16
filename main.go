b81f245c0a060a879a6ecb2388d5a3b565b9693c
Jonathan Torres <jonathantorres41@gmail.com>
2020-06-16 00:41:31 -0400

    Add functions to create main.go and gitignore files

--- main.go
+++ main.go
@@ -3,19 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"flag"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Println("usage: gonew [project]")
+	var gitignore = flag.Bool("gitignore", false, "Generate a .gitignore file")
+	var name string
+	flag.Parse()
+	if (len(flag.Args()) == 0) {
+		fmt.Println("usage: gonew [--options] [project_name]")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	var files []string = []string{
-		gitignoreText,
-		mainText,
-		readmeText,
+
+	name = flag.Args()[0]
+	createRootFolder(&name)
+	createMain(&name)
+	if (*gitignore) {
+		createGitIgnore(&name);
 	}
-	for _, file := range files {
-		fmt.Println(file)
+}
+
+func createRootFolder(name *string) {
+	if err := os.Mkdir(*name, 0777); err != nil {
+		fmt.Println("directory " + *name + " already exists!")
+	}
+}
+
+func createGitIgnore(path *string) {
+	file, err := os.Create(*path + "/.gitignore")
+	if (err != nil) {
+		fmt.Println("Could not create .gitignore file:", err)
+		return
+	}
+	_, err = file.Write([]byte(gitignoreText))
+	if (err != nil) {
+		fmt.Println("Could not write code to .gitignore file:", err)
+		return
+	}
+}
+
+func createMain(path *string) {
+	file, err := os.Create(*path + "/main.go")
+	if (err != nil) {
+		fmt.Println("Could not create main.go file:", err)
+		return
+	}
+	_, err = file.Write([]byte(mainText))
+	if (err != nil) {
+		fmt.Println("Could not write code to main.go file:", err)
+		return
 	}
 }
